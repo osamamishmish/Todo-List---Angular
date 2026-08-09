c4ec687b4754b47ae43ab0bec0a9c070d8bcd8ed
Osama Mishmish <osama.mishmish16@gmail.com>
2026-08-09 16:21:39 +0300

    modify styling of active component

--- src/app/Components/completed/completed.ts
+++ src/app/Components/completed/completed.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { Theme } from '../../Directives/theme';
+import { Styling } from '../../Services/styling';
 
 @Component({
   selector: 'app-completed',
-  imports: [],
+  imports: [Theme],
   templateUrl: './completed.html',
   styleUrl: './completed.css',
 })
 export class Completed {
+  completed =inject(Styling);
+  completedTodos=computed(()=>{
+    return this.completed.all().filter(todo=>todo.checked);
+  })
+  toggleTodo(index:number){
+      this.completed.all.update(all=>all.map((todo,i)=>i===index?{...todo,checked:!todo.checked}:todo));
+
+  }
 
 }
